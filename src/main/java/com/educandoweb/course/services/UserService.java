56e2478b0157e17280d2a101a62290d22d384e39
Carlos <lacerda.carlos@gmail.com>
2021-05-26 14:46:35 +0100

    Dev profile

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -48,6 +48,7 @@ public class UserService {
 	
 	public User update(Long id, User obj) {
 		try {
+		//	User entity = repository.getOne(id); /// Deprecated
 			User entity = repository.getById(id);
 			updateData(entity, obj);
 			return repository.save(entity);
